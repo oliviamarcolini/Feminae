c1331e6a91945a61b73dbf175f52f2087c3865ba
Olivia Marcolini <oliviamonteiro2406@gmail.com>
2025-07-15 12:25:37 -0300

    Initial Commit

---
+++ Feminae/FeminaeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FeminaeApp.swift
+//  Feminae
+//
+//  Created by Olivia Monteiro on 7/15/25.
+//
+
+import SwiftUI
+
+@main
+struct FeminaeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

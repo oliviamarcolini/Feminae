b04bc41f5b4ac91ff0df6004131007be1563b0b0
Gargi Tiwari <gargi.ktiwari@gmail.com>
2025-07-15 11:50:27 -0400

    Add 4 veiw pages

---
+++ Feminae/SelfCare.swift
@@ -0,0 +1,18 @@
+//
+//  SelfCare.swift
+//  Feminae
+//
+//  Created by Dhiraj Tiwari on 7/15/25.
+//
+
+import SwiftUI
+
+struct SelfCare: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SelfCare()
+}
